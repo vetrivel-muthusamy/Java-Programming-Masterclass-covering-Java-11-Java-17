9b9340dc6599909e8e7e5ae2916c09e6ebb972b9
vetrivel-muthusamy <vetrivel.muthusamy01@gmail.com>
2021-10-14 23:42:36 +0530

    Section3_23_Variables

---
+++ src/com/section3/variable/variable.java
@@ -0,0 +1,14 @@
+package com.section3.variable;
+
+public class variable {
+	public static void main(String[] args) {
+		
+		int myFirstNumber = 5;//Declaration Statement or Variable initialization
+		System.out.println("myFirstNumber" + myFirstNumber);
+		/*What is expression?
+		* This is a Construct that evaluates to a single value*/
+		
+		/*What is String Literal
+		* Any sequence of characters surrounded by double quotes is a string literal in java. It's value cannot be changed, Unlike a Variable*/
+	}
+}
